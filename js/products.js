a31cd650bbdcf54555ec927e0191e3a22bbc4079
Ahmed <medosamsam919@gmail.com>
2024-12-31 06:23:42 +0200

    fix product add styling

--- js/products.js
+++ js/products.js
@@ -1,14 +1,13 @@
 let Nav = document.getElementById("nav");
 
 // change backgroundColor of nav
-window.onscroll = function scroll() {
+window.onscroll= function scroll() {
   if (window.scrollY > 50) {
     Nav.classList.add("nav-scroll");
   } else {
     Nav.classList.remove("nav-scroll");
   }
-};
-
+}
 // change between login and logout
 const loginbtn = document.getElementById("loginbtn");
 const user_menu_button = document.getElementById("user-menu-button");
@@ -22,28 +21,30 @@ if (token) {
   user_menu_button.style.display = "none";
 }
 
+
 // signout
 signout.addEventListener("click", () => {
-  localStorage.removeItem("token");
-  localStorage.removeItem("user");
-  window.location = "index.html";
-});
-
-// user profile
-const user_photo = document.getElementById("user-photo");
-const user_name = document.getElementById("user-name");
-const user_email = document.getElementById("user-email");
-
-const user = JSON.parse(localStorage.getItem("user"));
-
-if (user_photo) {
-  user_photo.src = user.profile_image;
-}
-
-if (user_name) {
-  user_name.innerHTML = user.name;
-}
-
-if (user_email) {
-  user_email.innerHTML = user.email;
-}
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    window.location = "index.html";
+  });
+  
+  // user profile
+  const user_photo = document.getElementById("user-photo");
+  const user_name = document.getElementById("user-name");
+  const user_email = document.getElementById("user-email");
+  
+  const user = JSON.parse(localStorage.getItem("user"));
+  
+  if (user_photo) {
+    user_photo.src = user.profile_image;
+  }
+  
+  if (user_name) {
+    user_name.innerHTML = user.name;
+  }
+  
+  if (user_email) {
+    user_email.innerHTML = user.email;
+  }
+  
