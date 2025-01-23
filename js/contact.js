5e6c04595f9a4492e20f1c81658c9b9c98f72afc
Ahmed <medosamsam919@gmail.com>
2025-01-23 22:32:12 +0200

    add js to contact us

--- js/contact.js
+++ js/contact.js
@@ -28,6 +28,52 @@ signout.addEventListener("click", () => {
   window.location = "index.html";
 });
 
+const MessageForm = document.getElementById("send_message");
+const Name = document.getElementById("name");
+const Email = document.getElementById("email");
+const Message = document.getElementById("message");
+const NameError = document.getElementById("nameError");
+const emailError = document.getElementById("emailError");
+const messageError = document.getElementById("messageError");
+
+//  Validation
+const Regex =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+MessageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //   Client Validation
+  if (Name.value.trim() === "") {
+    NameError.textContent = "Please enter your Name";
+  } else if (Name.value.length < 5) {
+    NameError.textContent =
+      "Please Enter a valid Name between 5 to 20 characters";
+  } else {
+    NameError.textContent = "";
+  }
+
+  if (Email.value.trim() === "" || !Regex.test(Email.value)) {
+    emailError.textContent = "Please Enter a valid email";
+  } else {
+    emailError.textContent = "";
+  }
+
+  if (Message.value.trim() === "") {
+    messageError.textContent = "Please enter your Message";
+  } else {
+    messageError.textContent = "";
+  }
+
+  if (
+    Name.value.trim() == "" ||
+    Email.value.trim() == "" ||
+    Message.value.trim() == ""
+  ) {
+    return;
+  }
+});
+
 // user profile
 const user_photo = document.getElementById("user-photo");
 const user_name = document.getElementById("user-name");
